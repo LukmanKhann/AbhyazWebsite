c6ccdf57b4d678d334592b2c77698d8a15d8e258
Lukman <khanlukman233@gmail.com>
2023-09-18 17:08:38 +0530

    added link and modified some files

--- src/Components/Abhyaz.jsx
+++ src/Components/Abhyaz.jsx
@@ -1,6 +1,9 @@
+import { toast } from "react-toastify";
 import style from "./abhyaz.module.css";
 
 const Goodtea = () => {
+
+  
   return (
     <div>
       {/* <main id={style.heading}>
@@ -36,7 +39,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>Join our team as an App Developer and work closely with our development team to build and maintain mobile applications. Contribute to the design, development, and testing of mobile apps, enhancing the user experience and functionality. This role is perfect for individuals interested in mobile app development and eager to learn in a dynamic environment.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
       <section className={style.jobopening}>
@@ -47,7 +50,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>Explore the world of web development as a Web Developer. Collaborate with our team to create and maintain websites, optimize user interfaces, and troubleshoot issues. Gain valuable experience in HTML, CSS, JavaScript, and web development frameworks.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
       <section className={style.jobopening}>
@@ -58,7 +61,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>Be part of our Software Engineering team and work on real-world software projects. Gain exposure to the full software development lifecycle as you collaborate on design, coding, testing, and maintenance of software applications.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
       
       <section className={style.jobopening}>
@@ -69,7 +72,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>Join our Digital Marketing team to promote our business through various online channels. You'll work on creating and implementing digital marketing strategies, analyzing performance metrics, and enhancing our online presence.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
       <section className={style.jobopening}>
@@ -80,7 +83,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>Join our creative team as a Graphic Designer and use your design skills to create visually appealing marketing materials, advertisements, and branding elements that resonate with our audience.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
       <section className={style.jobopening}>
@@ -91,7 +94,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>If you have a passion for writing and technical knowledge, consider our Technical Content Writer - Documentation position. You'll create clear and concise technical documentation, user guides, and instructional materials.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
       <section className={style.jobopening}>
@@ -102,7 +105,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>Join our Event Management team to gain hands-on experience in planning and executing events. Coordinate logistics, manage guest lists, and ensure events run smoothly.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
       <section className={style.jobopening}>
@@ -113,7 +116,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>Join our creative team as a Creative Editor and be part of the content creation process. You'll review, edit, and enhance written and visual content to ensure it aligns with our brand's style and messaging.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
       <section className={style.jobopening}>
@@ -124,7 +127,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>If you have a knack for design and visual creativity, our Canva/Photoshop Designer position is a perfect fit. You will learn to create eye-catching graphics, banners, and visuals for various marketing and communication materials.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
       <section className={style.jobopening}>
@@ -135,7 +138,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>Join our Social Media Marketing team to learn the art of promoting businesses through social platforms. You'll create engaging content, plan and schedule posts, analyze metrics, and implement strategies to grow our online presence.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
       <section className={style.jobopening}>
@@ -146,7 +149,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>Dive into the world of search engine optimization (SEO) as an SEO Marketing Specialist. You'll work on optimizing website content, conducting keyword research, and analyzing SEO performance metrics.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
       <section className={style.jobopening}>
@@ -157,7 +160,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>Join our Email Marketing team to gain hands-on experience in crafting effective email campaigns. You'll assist in creating and sending targeted emails, analyzing email performance, and optimizing email marketing strategies.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
       <section className={style.jobopening}>
@@ -168,7 +171,7 @@ const Goodtea = () => {
         <p><strong>Job Type:</strong> Full-time</p>
         <p><strong>Description:</strong></p>
         <p>As a Content Marketing Specialist, you'll be part of the content creation and promotion process. You'll create blog posts, articles, and other written content and help promote it through various channels.</p>
-        <a href="https://www.abhyaz.com/jobs" className={style.applybutton}>Apply Now</a>
+        <a href="https://mtabtechnology.zohorecruit.com/jobs/Internship-for-Graduates" className={style.applybutton}>Apply Now</a>
       </section>
 
      
